7b5be76e45afedbe795652993d1071563dfe7134
tuliooassis <tulioassis@outlook.com>
2018-04-27 23:35:45 -0300

    Quase finalizado

--- fisica/client.sh
+++ fisica/client.sh
@@ -8,13 +8,14 @@ readonly SERVER_PORT=12345;
 readonly SERVER_IP=localhost;
 
 # fecha porta ao finalizar com CTRL+C
-trap 'escreveLog "Fechando a porta do cliente | port: ${PORT_LISTEN}"; fuser -k -n tcp "${PORT_LISTEN}"; escreveLog "Cliente finalizado"; exit' INT
+trap 'escreveLog "Finalizando cliente"; exit' INT
 
 escreveLog(){
     echo -n $(date) >> ${CLIENT_LOG};
     echo ": $*" >> ${CLIENT_LOG};
 }
 
+# código conversor retirado de https://unix.stackexchange.com/questions/98948/ascii-to-binary-and-binary-to-ascii-conversion-tools
 ordbin(){
   a=$(printf '%08d' "'$1")
   echo "obase=2; $a" | bc
@@ -24,14 +25,22 @@ converterAsciiParaBinario(){
    echo -n $* | while IFS= read -r -n1 char
     do
         result=$(ordbin $char | tr -d '\n')
+        if [ $result == "0" ]; then
+            echo "00100000";
+            continue
+        fi
         while [ ${#result} -lt 8 ]; do
             result="0$result"
         done
-	    echo $result
+	    echo "$result "
     done
 }
 
-junta(){ local IFS="$1"; shift; echo "$*"; }
+junta(){
+    local IFS="$1";
+    shift;
+    echo "$*";
+}
 
 criaQuadro (){
     v_mensagem=$*;
@@ -41,46 +50,37 @@ criaQuadro (){
 
         #Inicio do quadro (1 byte) com valor 10101011 que indica o início do quadro, alertando sobre a última chance de sincronizar.
         # o 11 alerta que o campo seguinte é o endereço de destino
-            v_inicioQuadro='10101011';
+        v_inicioQuadro='10101011';
     #Fim cabeçalho da camada física
 
-	#Endereço MAC de origem (17 bytes) é o endereço da camada de enlace do reme tente do pacote
-    v_endOrigem=$(cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address);
-    #echo "MAC de origem: ${v_endOrigem}";
+	#Endereço MAC de origem (12 bytes) é o endereço da camada de enlace do reme tente do pacote
+    v_endOrigem=$(cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address  | tr -d ':');
     v_endOrigemBin=$(converterAsciiParaBinario ${v_endOrigem});
     v_endOrigemBin=$(junta "" ${v_endOrigemBin});
 
-    #echo "MAC de origem binário: ${v_endOrigemBin}";
-
-
-    #Endereço MAC de destino (17 bytes) é o endereço da camada de enlace de destino que receberá o pacote
+    #Endereço MAC de destino (12 bytes) é o endereço da camada de enlace de destino que receberá o pacote
 	#se o IP do servidor for localhost, MAC de origem = MAC destino
 	if [ "$SERVER_IP" == "localhost" ]; then
 		v_endDestino=$v_endOrigem;
 	#se não for localhost pega MAC do outro pc
 	else
-		v_endDestino=$(arp ${SERVER_IP} -a | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
+		v_endDestino=$(arp ${SERVER_IP} -a | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | tr -d ':')
 	fi
-    #echo "MAC de destino: ${v_endDestino}";
     v_endDestinoBin=$(converterAsciiParaBinario ${v_endDestino});
     v_endDestinoBin=$(junta "" ${v_endDestinoBin});
-    #echo "MAC de destino binário: ${v_endDestinoBin}";
 
     #Tipo (2 bytes) possui o protocolo da camada superior cujo pacote está encapsulado no quadro. Ex.: IP
     v_tipo='0000000011111111'
 
     #Dados em binário
     v_dados=${v_mensagem};
-    #echo "Mensagem do quadro: $v_dados";
     v_dadosBin=$(converterAsciiParaBinario ${v_dados});
-    #echo "Mensagem do quadro binário1: $v_dadosBin";
-    #v_dadosBin=$(junta "" ${v_dadosBin});
-    #echo "Mensagem do quadro binário: $v_dadosBin";
+    v_dadosBin=$(junta "" ${v_dadosBin});
+
     #CRC: detecção de erros (4 bytes)
     v_crc='11111111111111111111111111111111';
 
     quadro="${v_preambulo}${v_inicioQuadro}${v_endDestinoBin}${v_endOrigemBin}${v_tipo}${v_dadosBin}${v_crc}";
-    #echo "Quadro completo: ${quadro}"
     echo $quadro;
 }
 
@@ -90,39 +90,22 @@ if [ -e ${CLIENT_LOG} ]; then
 fi
 
 escreveLog "Criando quadro"
-quadro=$(criaQuadro $*);
+cat $* > ${CLIENT_FILE}
+mensagem=$(cat $CLIENT_FILE)
+quadro=$(criaQuadro ${mensagem});
+echo ${quadro} > ${CLIENT_FILE}
 
-escreveLog "Iniciando cliente na porta: ${PORT_LISTEN} com arquivo: ${CLIENT_FILE}";
-nc -k -l "${PORT_LISTEN}" > "${CLIENT_FILE}" &
-
-escreveLog "Conectando no servidor de ip: ${SERVER_IP}, na porta: ${SERVER_PORT} e esperando por resposta";
-
-
-escreveLog "Aguardando o servidor ficar pronto"
-nc -z "${SERVER_IP}" "${SERVER_PORT}";
-isOpen=$?;
-while [ ! "${isOpen}" -eq 0 ];
-do
-    nc -z "${SERVER_IP}" "${SERVER_PORT}";
-    isOpen=$?;
+escreveLog "Tentando enviar o quadro"
+tentativa=$(( ( RANDOM % 10 )  + 1 ));
+while [ $((${tentativa}%2)) -eq 0 ]; do
+    escreveLog "Erro ao enviar, tentando novamente..."
+    tentativa=$(( ( RANDOM % 10 )  + 1 ));
 done
 
-# solicita TMQ para o servidor
-escreveLog "Solicita TMQ";
-echo "TMQ" | nc -q 2 "${SERVER_IP}" "${SERVER_PORT}";
-escreveLog "Recebe TMQ"
-TMQ=$(cat ${CLIENT_FILE});
-escreveLog "Fechando a porta do cliente | porta: ${PORT_LISTEN}";
-fuser -k -n tcp "${PORT_LISTEN}";
-escreveLog "Iniciando cliente na porta: ${PORT_LISTEN} com arquivo: ${CLIENT_FILE}";
-nc -I $TMQ -O $TMQ -k -l "${PORT_LISTEN}" | tee "${CLIENT_FILE}" &
+escreveLog "Eviando o quadro ${CLIENT_FILE} na porta ${PORT_LISTEN}";
+nc ${SERVER_IP} ${SERVER_PORT} < ${CLIENT_FILE};
 
-escreveLog "Envia quadro para o servidor"
-echo "${quadro}" | nc -q 2 "${SERVER_IP}" "${SERVER_PORT}";
+escreveLog "Quadro enviado com sucesso!"
 
-escreveLog "Envia finalizar para o servidor"
-echo "FIM" | nc -q 2 "${SERVER_IP}" "${SERVER_PORT}";
-
-escreveLog "Fechando a porta do cliente | porta: ${PORT_LISTEN}";
-fuser -k -n tcp "${PORT_LISTEN}";
+escreveLog "Finalizando o cliente";
 exit 0;
--- fisica/server.sh
+++ fisica/server.sh
@@ -2,26 +2,11 @@
 
 # define porta e arquivos de saída
 readonly PORT_LISTEN=12345;
-readonly PORT_LISTEN_CLIENT=54321;
 readonly SERVER_FILE=server.out;
 readonly SERVER_LOG=server.log;
-readonly TMQ=1;
-CLIENT_IP=localhost;
-CLIENT_PORT=localhost;
-# fecha porta ao finalizar com CTRL+C
-trap 'escreveLog "Fechando a porta do servidor | port: ${PORT_LISTEN}"; fuser -k -n tcp "${PORT_LISTEN}"; escreveLog "Servidor finalizado"; exit' INT
 
-obterIpCliente(){
-    OK=false;
-    while [ OK != "true" ]; do
-        tmpNetworkString=$(lsof -i:"${PORT_LISTEN}" | grep "localhost:${PORT_LISTEN} (ESTABLISHED)" | awk '{print $9}');
-        if [ -s "${SERVER_FILE}" ] && [ ! -z "${tmpNetworkString}" ]; then
-            OK=true;
-            CLIENT_IP=$(echo $tmpNetworkString | cut -d':' -f1);
-            CLIENT_PORT=$(echo $tmpNetworkString | cut -d'-' -f1 | cut -d':' -f2);
-        fi
-    done
-}
+# fecha porta ao finalizar com CTRL+C
+trap 'escreveLog "Finalizando servidor"; exit' INT
 
 escreveLog(){
     echo -n $(date) >> ${SERVER_LOG};
@@ -29,77 +14,55 @@ escreveLog(){
 }
 
 # código conversor retirado de https://unix.stackexchange.com/questions/98948/ascii-to-binary-and-binary-to-ascii-conversion-tools
-ordbin(){
-  a=$(printf '%d' "'$1")
-  echo "obase=2; $a" | bc
-}
-
 chrbin() {
         echo $(printf \\$(echo "ibase=2; obase=8; $1" | bc))
-}
-
-converterAsciiParaBinario(){
-   echo -n $* | while IFS= read -r -n1 char
-    do
-        result=$(ordbin $char | tr -d '\n')
-        echo $result
-       # echo -n " "
-    done
 }
 
 converterBinarioParaAscii() {
     for bin in $*
     do
-        result=$(chrbin $bin | tr -d '\n')
-	echo $result
-    done
+        for (( i = 0; i < ${#bin}; i+=8 )); do
+            conv=${bin:i:8};
+            result=$(chrbin $conv | tr -d '\n');
 
+            if [ $conv == "00100000" ]; then
+                palavra=$(junta "" ${palavra});
+                echo "$palavra"
+                unset palavra
+                continue;
+            fi
+            palavra=$palavra$result;
+        done
+        palavra=$(junta "" ${palavra});
+        echo $palavra
+    done
 }
 
-junta(){ local IFS="$1"; shift; echo "$*"; }
+junta(){
+    local IFS="$1";
+    shift;
+    echo "$*";
+}
 
 if [ -e ${SERVER_LOG} ]; then
     rm ${SERVER_LOG} ;
     escreveLog "Removendo arquivo antigo de log"
 fi
 
+escreveLog "Servidor iniciado"
+
 while true; do
-    escreveLog "Iniciando servidor na porta: ${PORT_LISTEN} com arquivo: ${SERVER_FILE}";
-    nc -I $TMQ -O $TMQ -k -l "${PORT_LISTEN}" | tee "${SERVER_FILE}" &
+    escreveLog "Aguardando conexão na porta ${PORT_LISTEN}";
+    nc -l -p ${PORT_LISTEN} > ${SERVER_FILE};
 
-    escreveLog "Aguardando pela conexão...";
+    escreveLog "Quadro recebido"
+    escreveLog "Obtendo mensagem recebida"
 
-    while true; do
-        content=$(cat $SERVER_FILE);
-        content=${content:0:8};
-        #echo "content ${content}";
-        case ${content} in
-            "TMQ")
-                #obterIpCliente;
-                escreveLog "Cliente conectado | ip: ${CLIENT_IP} porta: ${CLIENT_PORTA}";
-                escreveLog "Mensagem recebida: ${content}";
-                escreveLog "Enviando TMQ";
-                echo "$TMQ" | nc -q 2 "${CLIENT_IP}" "${PORT_LISTEN_CLIENT}";
-                ;;
-            "10101010")
-                escreveLog "Recebendo quadro";
-                v_mensagem=$(cat ${SERVER_FILE});
-                v_mensagem="${v_mensagem:352}";
-                v_mensagem="${v_mensagem:0:$((${#v_mensagem}-32))}";
-                v_mensagem=$(converterBinarioParaAscii $v_mensagem);
-		#printf "%s"  "${v_mensagem[*]}"
-		echo "Mensagem recebida:"
-		echo $v_mensagem
-                escreveLog "Mensagem recebida no quadro: $(echo $v_mensagem)";
-                echo "OK" | nc -q 2 "${CLIENT_IP}" "${PORT_LISTEN_CLIENT}";
-                ;;
-            "FIM")
-                escreveLog "Mensagem recebida: ${content}";
-                escreveLog "Cliente desconectado | porta: ${PORT_LISTEN}"
-                ;;
-            *)
-            ;;
-        esac
-        cat /dev/null > "${SERVER_FILE}";
-    done
+    quadro=$(cat ${SERVER_FILE})
+    mensagem=${quadro:272}
+    mensagem="${mensagem:0:$((${#mensagem}-32))}";
+    mensagem=$(converterBinarioParaAscii $mensagem);
+    escreveLog "Mensagem recebida no quadro: $(echo $mensagem)";
+
+    sleep 5
 done
